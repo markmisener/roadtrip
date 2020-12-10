48162f1a8bb1f3fb5839b07bf04a08ba3a4605f8
Mark Misener <markmisener@gmail.com>
2020-12-09 19:39:52 -0700

    Mm/sidebar (#7)
    * use a sidebar to show images and descriptions
    
    * add button for changing img, add imgs only if exists
    
    * slideshow in popups
    
    * use leaflet
    
    * add more locations
    
    * even more locations
    
    * whoops

--- server.py
+++ server.py
@@ -1,24 +1,49 @@
 """Starts the Flask app."""
 import json
 import os
+from pathlib import Path
+from typing import Optional
 
 from flask import Flask
 from flask import render_template
 
-
-# configure flask app
 app = Flask(__name__)
-app.config.from_envvar("APP_CONFIG_FILE", silent=True)
 MAPBOX_ACCESS_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN", None)
 
-# display at homepage path
+
+def get_file_contents(filename: str) -> Optional[str]:
+    """Get the contents of a file.
+
+    Args:
+        filename: path to file.
+
+    Return:
+        Optional[str]: file contents
+
+    """
+    try:
+        return open(filename, "r").read()
+    except FileNotFoundError:
+        return None
+
+
 @app.route("/")
 def display():
     """Displays the homepage."""
+    data = json.load(open("static/data/pointData.json"))
+    for feature in data["features"]:
+        city = feature["properties"]["city"]
+        feature["properties"]["images"] = list()
+        feature["properties"]["captions"] = list()
+
+        n_images = len([x for x in Path(f"static/data/cities/{city}").glob("*.jpg")])
+        for i in range(n_images):
+            feature["properties"]["images"].append(f"static/data/cities/{city}/{i}.jpg")
+            fname = f"static/data/cities/{city}/{i}.txt"
+            feature["properties"]["captions"].append(get_file_contents(fname))
+
     return render_template(
-        "index.html",
-        MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN,
-        data=json.load(open("static/data/pointData.json")),
+        "index.html", MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN, data=data
     )
 
 
--- static/js/mapping.js
+++ static/js/mapping.js
@@ -1,9 +1,111 @@
+STATE = {
+  "lastClickedDescriptionId": null,
+}
 
-function formatPointData(pointData) {
-  /* format for API request, lng,lat;lng,lat...
-  */
+function toggleVisibility(id) {
+   var e = document.getElementById(id);
+   if (e.style.display == 'block') {
+     e.style.display = 'none';
+   } else {
+     e.style.display = 'block';
+   }
+}
+
+function buildLocationList(data) {
+  pointData.features.forEach(function(location, i){
+    var prop = location.properties;
+
+    var listings = document.getElementById('listings');
+    var listing = listings.appendChild(document.createElement('div'));
+    listing.id = `listing-${prop.id}`;
+    listing.className = 'item';
+
+    var link = listing.appendChild(document.createElement('a'));
+    link.href = '#';
+    link.className = 'title';
+    link.id = `link-${prop.id}`;
+    link.innerHTML = prop.name;
+
+    var details = listing.appendChild(document.createElement('div'));
+    details.innerHTML = prop.lodging;
+
+    var description = details.appendChild(document.createElement("div"));
+    var descriptionId = `link-${prop.id}-description`;
+    description.setAttribute("id", descriptionId);
+    description.setAttribute("hidden", true);
+    description.innerHTML = prop.description;
+
+    link.addEventListener('click', function(e) {
+      for (var i = 0; i < data.features.length; i++) {
+        if (this.id === `link-${data.features[i].properties.id}`) {
+          var thisDescriptionId = `${this.id}-description`
+
+          // open clicked description
+          toggleVisibility(thisDescriptionId);
+
+          // close the last open description
+          if (STATE.lastClickedDescriptionId !== null & thisDescriptionId !== STATE.lastClickedDescriptionId) {
+            if (document.getElementById(STATE.lastClickedDescriptionId).style.display === 'block') {
+              toggleVisibility(STATE.lastClickedDescriptionId);
+            }
+          }
+
+          // center on marker, open popup
+          if (thisDescriptionId !== STATE.lastClickedDescriptionId) {
+            var clickedListing = data.features[i];
+            var coords = clickedListing.geometry.coordinates;
+            map.flyTo([coords[1] + 0.3, coords[0]], 10)
+            document.getElementById(`marker-${clickedListing.properties.id}`).click();
+
+            STATE.lastClickedDescriptionId = thisDescriptionId;
+          }
+        }
+      }
+    });
+  });
+}
+
+function generatePopupContent(feature) {
+  var images = feature.properties.images;
+  var captions = feature.properties.captions;
+  var slideshowContent = '';
+  for ( var i = 0; i < images.length; i++ ) {
+      slideshowContent += '<div class="image' + (i === 0 ? ' active' : '') + '">' +
+                            `<img src="${images[i]}" />` +
+                            `<div class="caption">${captions[i]}</div>` +
+                          '</div>';
+  }
+
+  var popupContent =  `<div id="${feature.properties.city}-popup" class="popup">` +
+                          `<h3>${feature.properties.name}</h3>` +
+                          `<div class="slideshow">${slideshowContent}</div>` +
+                          '<div class="cycle">' +
+                            '<a href="#" class="prev">&laquo; Previous</a>' +
+                            '<a href="#" class="next">Next &raquo;</a>' +
+                          '</div>'
+                      '</div>';
+
+  return popupContent
+}
+
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.popupContent) {
+      layer.bindPopup(feature.properties.popupContent, {
+        closeButton: true,
+        closeOnClick: true,
+        minWidth: 350,
+        offset: L.point(-5, -25)
+      })
+      layer.setIcon(new L.DivIcon({
+        className:"marker",
+        html:`<div id="marker-${feature.properties.id}"><span><b>${feature.properties.id}</b></span></div>`
+      }));
+  }
+}
+
+function formatPointData(f) {
   var coordinatesArray = [];
-  pointData.features.forEach(element => coordinatesArray.push(element.geometry.coordinates));
+  f.forEach(element => coordinatesArray.push(element.geometry.coordinates));
 
   var stringifiedCoordinatesArray = [];
   coordinatesArray.forEach(point => stringifiedCoordinatesArray.push(point.join(',')));
@@ -11,53 +113,44 @@ function formatPointData(pointData) {
   return stringifiedCoordinatesArray.join(';')
 }
 
-function requestData(requestUrl) {
-  /* make API request
-  */
-  return $.ajax({
-    method: 'GET',
-    url: requestUrl
-  });
-};
-
-function addDataToMap(map, pointData, geometry) {
-  /* add point data and route geometry to the map
-  */
-  map.addLayer({
-    "id": "route",
-    "type": "line",
-    "source": {
-      "type": "geojson",
-      "data": {
-        "type": "Feature",
-        "properties": {},
-        "geometry": geometry
-      }
-    },
-    "paint": {
-      "line-color": "#03AA46",
-      "line-width": 4,
-      "line-opacity": 0.5
+function handleRouteData(coordinates) {
+  var feature = {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "LineString",
+      "coordinates": coordinates
     }
-  });
+  }
 
-  pointData.features.forEach(function(marker, i) {
-    var el = document.createElement('div');
-    el.className = 'marker';
-    el.innerHTML = '<span><b>' + (i + 1) + '</b></span>'
+  L.geoJSON(feature).addTo(map)
+}
 
-    new mapboxgl.Marker(el)
-      .setLngLat(marker.geometry.coordinates)
-      .setPopup(new mapboxgl.Popup({
-          offset: 25
-        })
-        .setHTML('<h3>' + marker.properties.Name + '</h3>' +
-                 '<h4>' + marker.properties.Lodging + '</h4>'))
-      .addTo(map);
-  });
+function makeRoute(pointArr) {
+  var chunks = [];
+  for (var i=0; i < pointArr.length; i+= 25) {
+    if (i === 0) {
+      chunks.push(pointArr.slice(0, 25));
+    }
+    else if (i < pointArr.length - 25) {
+      chunks.push(pointArr.slice(i-1, i+25));
+    } else {
+      var arrWithHome = pointArr.slice(i-1, i+25).concat(pointArr[0]);
+      chunks.push(arrWithHome);
+    }
+  }
 
-  var bbox = turf.bbox(pointData);
-  map.fitBounds(bbox, {
-    padding: 50
-  });
+  for (var i=0; i < chunks.length; i++) {
+      var pointsString = formatPointData(chunks[i]);
+      var baseUrl = 'https://api.mapbox.com/directions/v5/mapbox/driving'
+      var requestUrl = `${baseUrl}/${pointsString}.json?access_token=${L.mapbox.accessToken}&geometries=geojson&overview=full`
+      var response = $.ajax({
+        method: 'GET',
+        dataType: "json",
+        url: requestUrl,
+        success: function(data) {
+          handleRouteData(data.routes[0].geometry.coordinates)
+        }
+      });
+  };
 }
